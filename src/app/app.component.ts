c7b6933a8d39e9a562ff34132441a5d9ab3861bf
Ruiz <gerald.ruiz@accenture.com>
2017-02-22 19:37:08 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,46 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  //zoom level 
+
+  zoom: number = 10;
+
+  //start position
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  markers: marker[] = [
+    {
+      name: 'Accenture Innovation Center', 
+      lat: 29.7535,
+      lng: 95.3657, 
+      draggable: true
+    },
+    {
+      name: 'Accenture 2', 
+      lat: 29.7435,
+      lng: 96.3657, 
+      draggable: true
+    },
+    {
+      name: 'Accenture 3', 
+      lat: 29.7268,
+      lng: 95.5657, 
+      draggable: true
+    },
+  ]
+
+  constructor() {
+
+  }
 }
+
+interface marker {
+  name?: string;
+  lat: number;
+  lng: number;
+  draggable: boolean;
+}
+
